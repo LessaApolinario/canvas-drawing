4324982b7fc8256bc4140163c2fbfbaff03b5ea5
Lessa Apolinario <lessaapolinario@gmail.com>
2023-03-08 16:29:45 -0300

    Reorganize functions order on js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -26,17 +26,35 @@ function fillBlueRectIfSupports(canvas) {
         fillBlueRect(context, 200, 100, 40, 40);
     }
 }
+function fillBlueRect(context, x, y, width, height) {
+    context.fillStyle = 'rgba(0, 0, 200, 0.5)';
+    context.fillRect(x, y, width, height);
+}
 function drawRedLineIfSupports(canvas) {
     if (supports(canvas)) {
         const context = canvas.getContext('2d', { alpha: false });
         drawRedLine(context, 50, 300);
     }
 }
+function drawRedLine(context, x, y) {
+    context.strokeStyle = 'rgb(201, 31, 14)';
+    context.beginPath();
+    context.moveTo(0, 0);
+    context.lineTo(x, y);
+    context.stroke();
+}
 function drawGreenArcIfSupports(canvas) {
     if (supports(canvas)) {
         const context = canvas.getContext('2d', { alpha: false });
         drawGreenArc(context, 200, 100, 30);
     }
+}
+function drawGreenArc(context, x, y, radius) {
+    context.strokeStyle = '#199E30';
+    context.beginPath();
+    context.arc(x, y, radius, 0, Math.PI * 2);
+    context.moveTo(170, 100);
+    context.stroke();
 }
 function clearIfSupports(canvas) {
     if (supports(canvas)) {
@@ -47,24 +65,6 @@ function clear(canvas) {
     const context = canvas.getContext('2d', { alpha: false });
     context.fillStyle = '#fff';
     context.fillRect(0, 0, canvas.width, canvas.height);
-}
-function fillBlueRect(context, x, y, width, height) {
-    context.fillStyle = 'rgba(0, 0, 200, 0.5)';
-    context.fillRect(x, y, width, height);
-}
-function drawRedLine(context, x, y) {
-    context.strokeStyle = 'rgb(201, 31, 14)';
-    context.beginPath();
-    context.moveTo(0, 0);
-    context.lineTo(x, y);
-    context.stroke();
-}
-function drawGreenArc(context, x, y, radius) {
-    context.strokeStyle = '#199E30';
-    context.beginPath();
-    context.arc(x, y, radius, 0, Math.PI * 2);
-    context.moveTo(170, 100);
-    context.stroke();
 }
 function setImageSrc(image, src) {
     image.src = src;
